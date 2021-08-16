d3826b20532ef26cbd1887a0bdc4de283a8b10ff
Mohaiminul Islam <cadetmohaimin1999@gmail.com>
2021-08-17 00:29:02 +0600

    section 12 completed

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -12,7 +11,7 @@ using Persistence;
 
 namespace API
 {
-    public class Program
+  public class Program
     {
         public static async Task Main(string[] args)
         {
@@ -24,8 +23,9 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
